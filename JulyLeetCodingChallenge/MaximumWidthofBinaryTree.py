06ff374b28fa4eb6cc28ab127221b3bd0f01c262
vidhi-mody <vidhimody6@gmail.com>
2020-07-10 17:40:36 +0530

    Day 9 of July LeetCoding Challenge

---
+++ JulyLeetCodingChallenge/MaximumWidthofBinaryTree.py
@@ -0,0 +1,74 @@
+"""
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        self.answer = [[0,0]]
+        def findWidth(l,p,r):
+            if r:
+                if len(self.answer)<=l:
+                    self.answer.append([p,p])
+                else:
+                    self.answer[l]=[min(self.answer[l][0],p),max(self.answer[l][1],p)]
+                findWidth(l+1,2*p,r.left)
+                findWidth(l+1,2*p+1,r.right)
+        findWidth(0,0,root)
+        return max(d[1] - d[0] + 1 for d in self.answer)
