64eda82a339ef3dca32069cf8747dcb95ef05b30
Vidhi Mody <49282105+vidhi-mody@users.noreply.github.com>
2020-07-19 16:22:05 +0530

    Create Cousins in Binary Tree.py

---
+++ MayLeetCodingChallenge/Cousins in Binary Tree.py
@@ -0,0 +1,55 @@
+"""
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
+
+Example 2:
+
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+
+Example 3:
+
+Input: root = [1,2,3,null,4], x = 2, y = 3
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree will be between 2 and 100.
+Each node has a unique integer value from 1 to 100.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        nodeValue = {}
+        def getOrder(node , level , parent):
+            if(not node):
+                return 0
+            nodeValue[node.val] = [level , parent]
+            getOrder(node.left , level + 1 , node.val)
+            getOrder(node.right , level + 1 , node.val)
+        getOrder(root , 0 , -1)
+        if((nodeValue[x][0] == nodeValue[y][0]) and (nodeValue[x][1] != nodeValue[y][1])):
+            return True
+        else:
+            return False
