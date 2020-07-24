161038fcd74b4f79d488bebef8470478b7524223
vidhi-mody <vidhimody6@gmail.com>
2020-07-24 18:24:19 +0530

    Day 23 of the July LeetCoding Challenge

---
+++ JulyLeetCodingChallenge/Single Number III.py
@@ -0,0 +1,21 @@
+"""
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+Example:
+
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+Note:
+
+The order of the result is not important. So in the above example, [5, 3] is also correct.
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+"""
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        answer = [nums[0]]
+        for i in range(1,len(nums)):
+            if(nums[i] in answer):
+                answer.remove(nums[i])
+            else:
+                answer.append(nums[i])
+        return answer
